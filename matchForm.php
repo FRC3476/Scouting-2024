1ac27dd83dfc1344e582674e7a52a8d9b16969eb
MihirYJoshi <mihiryj@gmail.com>
2024-02-18 14:30:43 -0800

    qr code

--- matchForm.php
+++ matchForm.php
@@ -179,10 +179,9 @@
                 <!--QR Code Scouting-->
                 <div class="tab-pane fade" id="qrcode-tab-pane" role="tabpanel" aria-labelledby="qrcode-tab" tabindex="0">
                   <br>
+                  <a>QR Code:</a>
+                  <br>
                   <div id="data-qr-code"></div>
-				          <div class="col-md-3">
-                    <button id="qrcode-tab" class="btn btn-primary">Submit</button>
-                  </div>
                 </div>
               </div>
             </div>
